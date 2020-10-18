f46a7be30d456907360b6e74858194b44b5e3641
kc900201 <nkc900201@gmail.com>
2020-10-18 16:20:57 +0900

    Javacsript tutorial - 10182020

--- javaScriptFundamentals/script.js
+++ javaScriptFundamentals/script.js
@@ -27,3 +27,42 @@ const add = (x, y) => {
 }
 
 console.log(add(3, 4));
+
+// Learning Scope (Global - Local)
+// --------------------------------
+
+let color = "Pink"; // global scope
+
+const favoriteColor = () => {
+    let color = "Pink"; // local scope
+    return "Hey, my favorite color is " + color; 
+}
+
+// console.log(color);
+
+// Static scope
+// --------------------
+
+// Scope A
+const testName = () => {
+    // Scope B
+    let name = "Lara";
+    const myOtherName = () => {
+        // Scope C
+    }
+}
+
+num = 0;
+
+while (num < 10) {
+    console.log(num);
+    num++;
+}
+
+num2 = 0;
+
+console.log("Do while loop");
+do {
+    console.log(num2);
+    num2;
+} while (num2 < 10)
